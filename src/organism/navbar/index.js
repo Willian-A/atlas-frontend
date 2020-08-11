3b8c679e559ec2b30e4b8eed824a935f31bc3c49
Willian Almeida <titangames.tg0@gmail.com>
2020-08-11 17:15:31 -0300

    navbar changes on login

--- src/organism/navbar/index.js
+++ src/organism/navbar/index.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
+import api from "../../service/api.js";
 
 import * as component from "./component";
 import MenuBar from "../../shared/images/icons/menu-bar";
 
 export default function Navbar() {
+  const [logged, setLogged] = useState(false);
+
+  async function getLoginStatus() {
+    try {
+      let response = await api.get("/logged");
+      setLogged(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
+  function UserButtons() {
+    if (logged) {
+      return (
+        <li>
+          <Link to="/logout">Sair</Link>
+        </li>
+      );
+    }
+    return (
+      <>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/cadastro">Cadastrar</Link>
+        </li>
+      </>
+    );
+  }
+
+  useEffect(() => {
+    getLoginStatus();
+  }, []);
+
   return (
     <component.NavbarContainer>
       <ul>
@@ -26,14 +63,7 @@ export default function Navbar() {
           <a href="/products">Produtos</a>
         </li>
       </ul>
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/cadastro">Cadastrar</Link>
-        </li>
-      </ul>
+      <ul>{UserButtons()}</ul>
     </component.NavbarContainer>
   );
 }
