570a0340cdf22bf8e3dfc2d308b0d773b5b211e5
Willian Almeida <titangames.tg0@gmail.com>
2020-11-09 19:37:23 -0300

    teste

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 const api = axios.create({
-  baseURL: "https://localhost:3333",
+  baseURL: "https://remake-backend-tcc.herokuapp.com:3333",
   withCredentials: true,
 });
 
