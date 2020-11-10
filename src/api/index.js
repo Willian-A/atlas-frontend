df6be05555bcba6efb3ab3c6fbca5eec85f2617a
Willian Almeida <titangames.tg0@gmail.com>
2020-11-10 15:36:30 -0300

    teste

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 const api = axios.create({
-  baseURL: "https://backend-tcc.vercel.app",
+  baseURL: "https://remake-backend-tcc.herokuapp.com",
   withCredentials: true,
+  headers: { "Content-Type": "application/x-www-form-urlencoded" },
 });
 
 export default api;
