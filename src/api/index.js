a0b206d91e1589efd1d550f5231fdb8a3d967621
Willian Almeida <titangames.tg0@gmail.com>
2020-12-05 00:59:36 -0300

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://localhost:3333",
+  baseURL: "https://remake-backend-tcc.herokuapp.com/",
   withCredentials: true,
 });
 
