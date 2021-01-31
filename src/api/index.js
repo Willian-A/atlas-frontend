9e69bb24a85510d1d7bb5cc9e2e06858fe6bd02f
Willian <titangames.tg0@gmail.com>
2021-01-31 18:35:53 -0300

    api update

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-let URL;
-if (process.env.NODE_ENV === "development") {
-  URL = "https://localhost:3333";
-} else {
-  URL = "https://remake-backend-tcc.herokuapp.com/";
-}
+let URL =
+  process.env.NODE_ENV === "dev"
+    ? "https://localhost:3333"
+    : "https://remake-backend-tcc.herokuapp.com";
 const api = axios.create({
   baseURL: URL,
   withCredentials: true,
