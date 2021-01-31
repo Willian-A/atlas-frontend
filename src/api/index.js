37a527a1b3a2e28636233e0d7856b0f1da257c94
Willian <titangames.tg0@gmail.com>
2021-01-31 19:55:49 -0300

    fixed env variables

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 let URL =
-  process.env.NODE_ENV === "dev"
-    ? "https://remake-backend-tcc.herokuapp.com"
-    : "https://localhost:3333";
+  process.env.ENV === "DEV"
+    ? "https://localhost:3333"
+    : "https://remake-backend-tcc.herokuapp.com";
 const api = axios.create({
   baseURL: URL,
   withCredentials: true,
