4e3ca9293e8afb2de482adb7e29257b1778bb655
Willian Almeida <willian.contatoal@gmail.com>
2021-05-17 19:03:03 -0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-let URL = "https://localhost:3333";
-//let URL = "https://remake-backend-tcc.herokuapp.com";
+// let URL = "https://localhost:3333";
+let URL = "https://remake-backend-tcc.herokuapp.com";
 const api = axios.create({
   baseURL: URL,
   withCredentials: true,
